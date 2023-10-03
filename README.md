f6bd6719377d6ab44d41066ad13f506ca91b6320
Osorkon21 <Osorkon21@users.noreply.github.com>
2023-10-03 09:52:13 -0500

    First branch commit, add links to HTML

--- README.md
+++ README.md
@@ -1,12 +1,1 @@
-HOMEWORK TIPS
 
-/*
-  1. Be thinking about how an array of objects might be very useful here
-  2. Use functions to control the overflow flow of things 
-  3. No questions/answers in the HTML
-  4. How can we generate each new question and its answers on demand 
-  5. How can we keep track of which question is on-screen
-  6. Get the quiz to work with 1-2 questions
-  7. Make the timer value very high to begin with
-  8. Console.log whatever you need for testing
-*/
---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+//   AS A coding boot camp student
+// I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+// SO THAT I can gauge my progress compared to my peers
+//   ```
+
+// ## Acceptance Criteria
+
+// ```
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
+
+// HOMEWORK TIPS
+
+//
+// 1. Be thinking about how an array of objects might be very useful here
+// 2. Use functions to control the overflow flow of things
+// 3. No questions/answers in the HTML
+// 4. How can we generate each new question and its answers on demand
+// 5. How can we keep track of which question is on-screen
+// 6. Get the quiz to work with 1-2 questions
+// 7. Make the timer value very high to begin with
+// 8. Console.log whatever you need for testing
+//
